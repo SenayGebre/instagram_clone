24a35e2ef7c91f56e62e695bab02af4218179ee3
Senay Gebre <sg.senay@gmail.com>
2022-06-26 10:16:43 -0400

    setup firebase

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
+import LandingScreen from './components/auth/Landing';
 import { NavigationContainer } from  "@react-navigation/native";
 import { createStackNavigator } from  "@react-navigation/stack";
-import LandingScreen from './components/auth/Landing';
-import * as firebase from 'firebase';
+import { firebase } from "./src/firebase/config";
 import 'react-native-gesture-handler';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBa0SCgZfqlVRR1rKIU7t-3DcBxKdQ1fqg",
-  authDomain: "instagram-dev-706d0.firebaseapp.com",
-  projectId: "instagram-dev-706d0",
-  storageBucket: "instagram-dev-706d0.appspot.com",
-  messagingSenderId: "801572028538",
-  appId: "1:801572028538:web:b86bc24060f88cb4867bc3"
-};
 
 const Stack = createStackNavigator();
 export default function App() {
